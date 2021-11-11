13dc7b702446ea6c42d254a8c13250d6bdb8e325
lucasserra3 <serralucas26@gmail.com>
2021-11-11 09:14:54 -0300

    update codebase

--- mi-primer-app-react/src/App.js
+++ mi-primer-app-react/src/App.js
@@ -1,38 +1,34 @@
-import "./App.css";
-import React from "react";
-import Carousel from "@brainhubeu/react-carousel";
-import "@brainhubeu/react-carousel/lib/style.css";
-import imageOne from "./1.png";
-import imageTwo from "./2.png";
-import imageThree from "./3.png";
-import imageFour from "./4.png";
-import imageFive from "./5.png";
-import imageSix from "./6.png";
-import imageSeven from "./7.png";
-import imageEight from "./8.png";
-import imageNine from "./9.png";
-import imageTen from "./10.png";
+import './App.css';
+import React from 'react';
+import Carousel from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import imageOne from "./1.png"
+import imageTwo from "./2.png"
+import imageThree from "./3.png"
+import imageFour from "./4.png"
+import imageFive from "./5.png"
+import imageSix from "./6.png"
+import imageSeven from "./7.png"
+import imageEight from "./8.png"
+import imageNine from "./9.png"
+import imageTen from "./10.png"
 import izquierda from "./FLECHAIZQ.png";
 import derecha from "./FLECHADER.png";
 
-const listaDeImagenes = [
-  { src: imageOne, className: "imageOne" },
-  { src: imageTwo, className: "imageTwo" },
-  { src: imageThree, className: "imageThree" },
-  { src: imageFour, className: "imageFour" },
-  { src: imageFive, className: "imageFive" },
-  { src: imageSix, className: "imageSix" },
-  { src: imageSeven, className: "imageSeven" },
-  { src: imageEight, className: "imageEight" },
-  { src: imageNine, className: "imageNine" },
-  { src: imageTen, className: "imageTen" },
-];
+
 
 const MyCarousel = () => (
-  <Carousel plugins={["arrows"]}>
-    {listaDeImagenes.map(({ src, className }) => (
-      <img src={src} className={className} />
-    ))}
+  <Carousel plugins={['arrows']}>
+    <img src={imageOne} className="imageOne"/>
+    <img src={imageTwo} className="imageTwo"/>
+    <img src={imageThree} className="imageThree"/>
+    <img src={imageFour} className="imageFour"/>
+    <img src={imageFive} className="imageFive"/>
+    <img src={imageSix} className="imageSix"/>
+    <img src={imageSeven} className="imageSeven"/>
+    <img src={imageEight} className="imageEight"/>
+    <img src={imageNine} className="imageNine"/>
+    <img src={imageTen} className="imageTen"/>
   </Carousel>
 );
 
